69919727dd36b99a0c14fbaa31437b980a00da66
TaoTran224 <trannhutao.224@gmail.com>
2026-04-16 17:33:33 +0700

    8nd

--- Image Classification/Form1.Designer.cs
+++ Image Classification/Form1.Designer.cs
@@ -33,15 +33,17 @@ namespace Image_Classification
             this.Classify = new System.Windows.Forms.TabPage();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.Training = new System.Windows.Forms.TabPage();
-            this.Btn_Training = new System.Windows.Forms.Button();
-            this.Btn_SelectTrainingFolder = new System.Windows.Forms.Button();
-            this.Btn_Convert = new System.Windows.Forms.Button();
-            this.Btn_SelectInput = new System.Windows.Forms.Button();
-            this.Btn_Output = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.Txt_DirInput = new System.Windows.Forms.TextBox();
             this.Txt_DirOutput = new System.Windows.Forms.TextBox();
+            this.Txt_DirInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Btn_Output = new System.Windows.Forms.Button();
+            this.Btn_SelectInput = new System.Windows.Forms.Button();
+            this.Btn_Convert = new System.Windows.Forms.Button();
+            this.Btn_SelectTrainingFolder = new System.Windows.Forms.Button();
+            this.Btn_Training = new System.Windows.Forms.Button();
+            this.Txt_ConvertStatus = new System.Windows.Forms.TextBox();
+            this.Convert_TrainingStatus = new System.Windows.Forms.TextBox();
             this.Classify.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.Training.SuspendLayout();
@@ -79,6 +81,8 @@ namespace Image_Classification
             // 
             // Training
             // 
+            this.Training.Controls.Add(this.Convert_TrainingStatus);
+            this.Training.Controls.Add(this.Txt_ConvertStatus);
             this.Training.Controls.Add(this.Txt_DirOutput);
             this.Training.Controls.Add(this.Txt_DirInput);
             this.Training.Controls.Add(this.label2);
@@ -96,43 +100,41 @@ namespace Image_Classification
             this.Training.Text = "Training";
             this.Training.UseVisualStyleBackColor = true;
             // 
-            // Btn_Training
+            // Txt_DirOutput
             // 
-            this.Btn_Training.Location = new System.Drawing.Point(6, 491);
-            this.Btn_Training.Name = "Btn_Training";
-            this.Btn_Training.Size = new System.Drawing.Size(85, 23);
-            this.Btn_Training.TabIndex = 1;
-            this.Btn_Training.Text = "Training";
-            this.Btn_Training.UseVisualStyleBackColor = true;
+            this.Txt_DirOutput.Enabled = false;
+            this.Txt_DirOutput.Location = new System.Drawing.Point(6, 62);
+            this.Txt_DirOutput.Multiline = true;
+            this.Txt_DirOutput.Name = "Txt_DirOutput";
+            this.Txt_DirOutput.Size = new System.Drawing.Size(294, 72);
+            this.Txt_DirOutput.TabIndex = 9;
             // 
-            // Btn_SelectTrainingFolder
+            // Txt_DirInput
             // 
-            this.Btn_SelectTrainingFolder.Location = new System.Drawing.Point(6, 296);
-            this.Btn_SelectTrainingFolder.Name = "Btn_SelectTrainingFolder";
-            this.Btn_SelectTrainingFolder.Size = new System.Drawing.Size(85, 23);
-            this.Btn_SelectTrainingFolder.TabIndex = 2;
-            this.Btn_SelectTrainingFolder.Text = "Select";
-            this.Btn_SelectTrainingFolder.UseVisualStyleBackColor = true;
-            this.Btn_SelectTrainingFolder.Click += new System.EventHandler(this.Btn_SelectTrainingFolder_Click);
+            this.Txt_DirInput.Enabled = false;
+            this.Txt_DirInput.Location = new System.Drawing.Point(6, 169);
+            this.Txt_DirInput.Multiline = true;
+            this.Txt_DirInput.Name = "Txt_DirInput";
+            this.Txt_DirInput.Size = new System.Drawing.Size(294, 70);
+            this.Txt_DirInput.TabIndex = 8;
             // 
-            // Btn_Convert
+            // label2
             // 
-            this.Btn_Convert.Location = new System.Drawing.Point(6, 245);
-            this.Btn_Convert.Name = "Btn_Convert";
-            this.Btn_Convert.Size = new System.Drawing.Size(85, 23);
-            this.Btn_Convert.TabIndex = 3;
-            this.Btn_Convert.Text = "Convert";
-            this.Btn_Convert.UseVisualStyleBackColor = true;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Dir";
             // 
-            // Btn_SelectInput
+            // label1
             // 
-            this.Btn_SelectInput.Location = new System.Drawing.Point(6, 140);
-            this.Btn_SelectInput.Name = "Btn_SelectInput";
-            this.Btn_SelectInput.Size = new System.Drawing.Size(51, 23);
-            this.Btn_SelectInput.TabIndex = 4;
-            this.Btn_SelectInput.Text = "Input";
-            this.Btn_SelectInput.UseVisualStyleBackColor = true;
-            this.Btn_SelectInput.Click += new System.EventHandler(this.Btn_SelectInput_Click);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Dir";
             // 
             // Btn_Output
             // 
@@ -144,41 +146,63 @@ namespace Image_Classification
             this.Btn_Output.UseVisualStyleBackColor = true;
             this.Btn_Output.Click += new System.EventHandler(this.Btn_Output_Click);
             // 
-            // label1
+            // Btn_SelectInput
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(63, 43);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(20, 13);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Dir";
+            this.Btn_SelectInput.Location = new System.Drawing.Point(6, 140);
+            this.Btn_SelectInput.Name = "Btn_SelectInput";
+            this.Btn_SelectInput.Size = new System.Drawing.Size(51, 23);
+            this.Btn_SelectInput.TabIndex = 4;
+            this.Btn_SelectInput.Text = "Input";
+            this.Btn_SelectInput.UseVisualStyleBackColor = true;
+            this.Btn_SelectInput.Click += new System.EventHandler(this.Btn_SelectInput_Click);
             // 
-            // label2
+            // Btn_Convert
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(63, 150);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(20, 13);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "Dir";
+            this.Btn_Convert.Location = new System.Drawing.Point(6, 245);
+            this.Btn_Convert.Name = "Btn_Convert";
+            this.Btn_Convert.Size = new System.Drawing.Size(85, 23);
+            this.Btn_Convert.TabIndex = 3;
+            this.Btn_Convert.Text = "Convert";
+            this.Btn_Convert.UseVisualStyleBackColor = true;
+            this.Btn_Convert.Click += new System.EventHandler(this.Btn_Convert_Click);
             // 
-            // Txt_DirInput
+            // Btn_SelectTrainingFolder
             // 
-            this.Txt_DirInput.Enabled = false;
-            this.Txt_DirInput.Location = new System.Drawing.Point(6, 169);
-            this.Txt_DirInput.Multiline = true;
-            this.Txt_DirInput.Name = "Txt_DirInput";
-            this.Txt_DirInput.Size = new System.Drawing.Size(182, 70);
-            this.Txt_DirInput.TabIndex = 8;
+            this.Btn_SelectTrainingFolder.Location = new System.Drawing.Point(6, 398);
+            this.Btn_SelectTrainingFolder.Name = "Btn_SelectTrainingFolder";
+            this.Btn_SelectTrainingFolder.Size = new System.Drawing.Size(85, 23);
+            this.Btn_SelectTrainingFolder.TabIndex = 2;
+            this.Btn_SelectTrainingFolder.Text = "Select";
+            this.Btn_SelectTrainingFolder.UseVisualStyleBackColor = true;
+            this.Btn_SelectTrainingFolder.Click += new System.EventHandler(this.Btn_SelectTrainingFolder_Click);
             // 
-            // Txt_DirOutput
+            // Btn_Training
             // 
-            this.Txt_DirOutput.Enabled = false;
-            this.Txt_DirOutput.Location = new System.Drawing.Point(6, 62);
-            this.Txt_DirOutput.Multiline = true;
-            this.Txt_DirOutput.Name = "Txt_DirOutput";
-            this.Txt_DirOutput.Size = new System.Drawing.Size(182, 72);
-            this.Txt_DirOutput.TabIndex = 9;
+            this.Btn_Training.Location = new System.Drawing.Point(3, 303);
+            this.Btn_Training.Name = "Btn_Training";
+            this.Btn_Training.Size = new System.Drawing.Size(85, 23);
+            this.Btn_Training.TabIndex = 1;
+            this.Btn_Training.Text = "Training";
+            this.Btn_Training.UseVisualStyleBackColor = true;
+            this.Btn_Training.Click += new System.EventHandler(this.Btn_Training_Click);
+            // 
+            // Txt_ConvertStatus
+            // 
+            this.Txt_ConvertStatus.Enabled = false;
+            this.Txt_ConvertStatus.Location = new System.Drawing.Point(94, 245);
+            this.Txt_ConvertStatus.Multiline = true;
+            this.Txt_ConvertStatus.Name = "Txt_ConvertStatus";
+            this.Txt_ConvertStatus.Size = new System.Drawing.Size(206, 52);
+            this.Txt_ConvertStatus.TabIndex = 11;
+            // 
+            // Convert_TrainingStatus
+            // 
+            this.Convert_TrainingStatus.Enabled = false;
+            this.Convert_TrainingStatus.Location = new System.Drawing.Point(94, 305);
+            this.Convert_TrainingStatus.Multiline = true;
+            this.Convert_TrainingStatus.Name = "Convert_TrainingStatus";
+            this.Convert_TrainingStatus.Size = new System.Drawing.Size(206, 52);
+            this.Convert_TrainingStatus.TabIndex = 12;
             // 
             // Form1
             // 
@@ -187,7 +211,7 @@ namespace Image_Classification
             this.ClientSize = new System.Drawing.Size(1264, 681);
             this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Classification";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Classify.ResumeLayout(false);
             this.tabControl1.ResumeLayout(false);
@@ -212,6 +236,8 @@ namespace Image_Classification
         private System.Windows.Forms.Button Btn_Output;
         private System.Windows.Forms.Button Btn_SelectInput;
         private System.Windows.Forms.Button Btn_Convert;
+        private System.Windows.Forms.TextBox Convert_TrainingStatus;
+        private System.Windows.Forms.TextBox Txt_ConvertStatus;
     }
 }
 
--- Image Classification/Form1.cs
+++ Image Classification/Form1.cs
@@ -7,29 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
-
-using System.IO;                    // Để làm việc với thư mục (Directory) và file (Path)
-using Emgu.CV;                      // Thư viện gốc OpenCV (CvInvoke, Mat...)
-using Emgu.CV.Structure;            // Để dùng Image<Gray, byte> hoặc Bgr
-using Emgu.CV.ML;                   // Thư viện Machine Learning (KNearest, TrainData)
-using Emgu.CV.CvEnum;               // Các hằng số (Inter.Linear, ThresholdType...)
-using System.Drawing.Imaging;       // Để hỗ trợ chuyển đổi Bitmap sang dữ liệu OpenCV
+using System.IO; 
+using OpenCvSharp;
+using OpenCvSharp.ML;
+using OpenCvSharp.Extensions;
+using System.Drawing.Imaging;
 using Microsoft.WindowsAPICodePack.Dialogs;
+
+using Size = OpenCvSharp.Size;
+using Point = OpenCvSharp.Point;
+using Rect = OpenCvSharp.Rect;
 
 namespace Image_Classification
 {
     public partial class Form1 : Form
     {
-        KNearest knn = new KNearest();
-        Matrix<float> trainData;
-        Matrix<float> trainLabels;
+        UInt16 IMAGE_SIZE = 28;
+
         string modelPath = Application.StartupPath + "\\trained_model.xml";
 
         string pathInput = "";
         string pathOutput = "";
+        string pathPixelData = "";
 
-        FolderBrowserDialog fbd = new FolderBrowserDialog();
         public Form1()
         {
             InitializeComponent();
@@ -68,7 +68,7 @@ namespace Image_Classification
                 // Lưu vào biến để dùng cho việc convert
                 pathOutput = folderPath;
             }
- 
+
         }
 
         private void Btn_SelectInput_Click(object sender, EventArgs e)
@@ -93,6 +93,135 @@ namespace Image_Classification
                 // Lưu vào biến để dùng cho việc convert
                 pathInput = folderPath;
             }
+        }
+
+
+        private void ProcessImages(string inputDir, string outputDir)
+        {
+            // Quét tất cả file ảnh (.jpg, .png)
+            var files = Directory.GetFiles(inputDir, "*.*", SearchOption.AllDirectories);
+
+            foreach (string file in files)
+            {
+                try
+                {
+                    // 1. Đọc ảnh Grayscale
+                    using (Mat src = new Mat(file, ImreadModes.Grayscale))
+                    using (Mat binary = new Mat())
+                    {
+                        // 2. Nhị phân hóa (Chữ trắng trên nền đen)
+                        // Dùng Otsu để tự động tìm ngưỡng tối ưu
+                        Cv2.Threshold(src, binary, 0, 255, ThresholdTypes.BinaryInv | ThresholdTypes.Otsu);
+
+                        // 3. Resize và căn giữa (Padding) để thành 28x28
+                        using (Mat finalImg = ResizeWithPadding(binary, IMAGE_SIZE, IMAGE_SIZE))
+                        {
+                            // 4. Lưu vào thư mục tương ứng với nhãn (tên thư mục cha)
+                            string label = Path.GetFileName(Path.GetDirectoryName(file));
+                            string saveDir = Path.Combine(outputDir, label);
+
+                            if (!Directory.Exists(saveDir)) Directory.CreateDirectory(saveDir);
+
+                            finalImg.SaveImage(Path.Combine(saveDir, Path.GetFileName(file)));
+                        }
+                    }
+                }
+                catch { /* Bỏ qua file lỗi hoặc không phải định dạng ảnh */ }
+            }
+        }
+
+        private Mat ResizeWithPadding(Mat src, int width, int height)
+        {
+            Mat result = new Mat(new OpenCvSharp.Size(width, height), MatType.CV_8UC1, Scalar.Black);
+
+            // Tính toán tỉ lệ để resize mà không làm méo chữ
+            double scale = Math.Min((double)width / src.Width, (double)height / src.Height);
+            int newW = (int)(src.Width * scale);
+            int newH = (int)(src.Height * scale);
+
+            using (Mat resized = new Mat())
+            {
+                Cv2.Resize(src, resized, new OpenCvSharp.Size(newW, newH));
+
+                // Tính vị trí để đặt ảnh vào giữa khung đen
+                int x = (width - newW) / 2;
+                int y = (height - newH) / 2;
+
+                // Chép ảnh đã resize vào giữa ma trận kết quả
+                Rect roi = new Rect(x, y, newW, newH);
+                resized.CopyTo(new Mat(result, roi));
+            }
+
+            return result;
+        }
+
+        private async void Btn_Convert_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(pathInput) || string.IsNullOrEmpty(pathOutput))
+            {
+                MessageBox.Show("Vui lòng chọn đường dẫn Input và Output!");
+                return;
+            }
+
+            Btn_Convert.Enabled = false;
+
+            await Task.Run(() =>
+            {
+                try
+                {
+                    // Lấy tất cả file ảnh, bất kể chữ hoa hay chữ thường
+                    var allFiles = Directory.GetFiles(pathInput, "*.*", SearchOption.AllDirectories)
+                                            .Where(s => s.EndsWith(".png", StringComparison.OrdinalIgnoreCase) ||
+                                                        s.EndsWith(".jpg", StringComparison.OrdinalIgnoreCase));
+
+                    foreach (string file in allFiles)
+                    {
+                        // Sử dụng khối using để giải phóng tài nguyên C++ ngay lập tức
+                        using (Mat src = new Mat(file, ImreadModes.Grayscale))
+                        {
+                            if (src.Empty()) continue;
+
+                            using (Mat binary = new Mat())
+                            {
+                                // 1. Nhị phân hóa tự động bằng Otsu
+                                Cv2.Threshold(src, binary, 0, 255, ThresholdTypes.BinaryInv | ThresholdTypes.Otsu);
+
+                                // 2. Resize và thêm lề (Padding) để không làm méo chữ số
+                                using (Mat finalImg = ResizeAndCenter(binary, 28, 28))
+                                {
+                                    // 3. Tạo cấu trúc thư mục đích (0, 1, 2...)
+                                    string label = Path.GetFileName(Path.GetDirectoryName(file));
+                                    string saveDir = Path.Combine(pathOutput, label);
+
+                                    if (!Directory.Exists(saveDir))
+                                        Directory.CreateDirectory(saveDir);
+
+                                    // 4. Lưu ảnh
+                                    string savePath = Path.Combine(saveDir, Path.GetFileName(file));
+                                    finalImg.SaveImage(savePath);
+                                }
+                            }
+                        }
+                        // Ép giải phóng bộ nhớ sau mỗi ảnh để tránh lỗi Type Initializer do tràn RAM
+                        GC.Collect();
+                        GC.WaitForPendingFinalizers();
+                    }
+
+                    this.Invoke(new Action(() => MessageBox.Show("Convert hoàn tất!")));
+                }
+                catch (Exception ex)
+                {
+                    this.Invoke(new Action(() => MessageBox.Show("Lỗi thực thi: " + ex.Message)));
+                }
+            });
+
+            btnConvert.Enabled = true;
+        }
+
+
+        private void Btn_Training_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
